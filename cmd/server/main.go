550c44f1746de1f318009b7213419f771f5cd2ef
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-17 11:38:20 +0330

    .env loading logic and auth routes registeration added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/sina-byn/go-jwt-auth/pkg/auth"
 	"github.com/sina-byn/go-jwt-auth/pkg/db"
 	"github.com/sina-byn/go-jwt-auth/pkg/user"
 
@@ -8,12 +12,19 @@ import (
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Failed to load environment variables from \".env\"")
+	}
+
 	db.Connect()
 	defer db.DB.Close()
 
 	r := gin.Default()
 
 	user.RegisterRoutes(r)
+	auth.RegisterRoutes(r)
 
 	r.Run(":8080")
 }
