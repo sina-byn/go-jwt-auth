b98e392516f850f1b891c3cd6e7b3286839b0bab
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-17 13:16:48 +0330

    GenerateToken util changed to GenerateTokenPair

--- pkg/utils/token.go
+++ pkg/utils/token.go
@@ -1,20 +1,51 @@
 package utils
 
 import (
+	"errors"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func GenerateToken(userId int64, email string) (string, error) {
-	jwtSecret := os.Getenv("JWT_SECRET")
+type TokenPair struct {
+	AccessToken  string
+	RefreshToken string
+}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+func GenerateTokenPair(userId int64, email string) (*TokenPair, error) {
+	jwtAccessSecret := os.Getenv("JWT_ACCESS_SECRET")
+	jwtRefreshSecret := os.Getenv("JWT_REFRESH_SECRET")
+
+	if jwtAccessSecret == "" || jwtRefreshSecret == "" {
+		return nil, errors.New("JWT secrets are not set in environment variables")
+	}
+
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userId": userId,
-		"exp":    time.Now().Add(time.Hour * 2).Unix(),
+		"exp":    time.Now().Add(time.Minute * 15).Unix(),
 	})
 
-	return token.SignedString([]byte(jwtSecret))
+	signedAccessToken, err := accessToken.SignedString([]byte(jwtAccessSecret))
+
+	if err != nil {
+		return nil, err
+	}
+
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	signedRefreshToken, err := refreshToken.SignedString([]byte(jwtRefreshSecret))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &TokenPair{
+		AccessToken:  signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}, nil
 }
