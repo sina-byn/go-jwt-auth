45f7cfdec952e2e49dbc6a65fd45bde245f0a576
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-17 11:52:08 +0330

    UserResponse struct added to exclude the Password field from api response

--- pkg/user/model.go
+++ pkg/user/model.go
@@ -1,8 +1,14 @@
 package user
 
 type User struct {
-	Id       int64 `json:"id"`
+	Id       int64  `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	Fullname string `json:"fullname"`
+}
+
+type UserResponse struct {
+	Id       int64  `json:"id"`
+	Email    string `json:"email"`
 	Fullname string `json:"fullname"`
 }
